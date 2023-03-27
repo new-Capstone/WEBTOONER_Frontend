bb0b9f37a01545c07e4c78d444d789ec36f5bd96
Lee_Laptop <leejinwook69@gmail.com>
2023-03-27 22:11:14 +0900

    about page layout

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 
 //Switch와 Route를 이용해 여러 페이지 간 이동을 구현할 수 있음
-import { BrowserRouter as router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Main from './pages/Main';
 import About from './pages/About';
 
 function App() {
   return (
-    <router>
-      <Switch>
+      <Routes>
         {/* <Route/>를 이용해 페이지를 추가할 수 있음 */}
-        <Route exact path="/" component={Main} />
-        <Route path="/about" component={About} />
-      </Switch>
-    </router>
+        <Route exact path="/" element={<Main />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
   );
 }
 
