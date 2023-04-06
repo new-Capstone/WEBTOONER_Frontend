b97b70ba8187824d7d36e6ea1e2145ccce55a811
garamG <109083994+garamG@users.noreply.github.com>
2023-04-06 16:25:51 +0900

    튜터찾기 페이지 css

--- src/ga/pages/FindTutor.js
+++ src/ga/pages/FindTutor.js
@@ -7,36 +7,35 @@ function FindTutor () {
     return (
         <div classname= "container">
             <Header /> 
-            //버튼 눌렀을때 각각의 카테고리 별 itemlist 나올 수 있게 
             <div classname="category_list">
                 <Link to='/tutorpage'>
-                    <button>느와르</button>
+                    <button class="bnt_item">느와르</button>
                 </Link>
                 <Link to='/tutorpage'>
-                    <button>로맨스</button>
+                    <button class="bnt_item">로맨스</button>
                 </Link>
                 <Link to='/tutorpage'>
-                    <button>액션</button>
+                    <button class="bnt_item">액션</button>
                 </Link>
                 <Link to='/tutorpage'>
-                    <button>호러</button>
+                    <button class="bnt_item">호러</button>
                 </Link>
             </div>
 
             <div classname="content">
                 <div classname="content_first">
                     <Link to='/tutorpage'>
-                        <img src="https://via.placeholder.com/250*340"/>
+                        <img src="https://via.placeholder.com/250*340"width="340" height="250"/>
                     </Link>
                 </div>
                 <div classname="content_second">
                     <Link to='/tutorpage'>
-                        <img src="https://via.placeholder.com/250*340"/>
+                        <img src="https://via.placeholder.com/250*340"width="340" height="250"/>
                     </Link>
                 </div>
                 <div classname="content_thrid">
                     <Link to='/tutorpage'>
-                        <img src="https://via.placeholder.com/250*340"/>
+                        <img src="https://via.placeholder.com/250*340" width="340" height="250"/>
                     </Link>
                 </div>
 
@@ -44,7 +43,7 @@ function FindTutor () {
             //다음 페이지로 연결 어떻게 해야하는지 .. 
             <div>
                 <Link to='/next_page'>
-                <button >다음페이지</button>
+                <button class="bnt_finish" >다음페이지</button>
                 </Link>
             </div>
         </div>
