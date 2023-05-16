77969c5263038f030297d78abc9f0df9598c7fed
garamG <109083994+garamG@users.noreply.github.com>
2023-05-16 16:26:27 +0900

    튜터 찾기 페이지 수정

--- src/ga/pages/FindTutor.js
+++ src/ga/pages/FindTutor.js
@@ -1,48 +1,20 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Header from '../components/Header';
-import '../styles/FindTutor.css';
+import React, { useState } from 'react';
+import TabBar from '../components/Tabbar';
+import TutorImage from '../components/TutorImage';
 
-function FindTutor () {
-    return (
-        <div>
-            <Header /> 
-            <div className="category_list">
-                <Link to='/tutorpage'>
-                    <button className="bnt_item">느와르</button>
-                </Link>
-                <Link to='/tutorpage'>
-                    <button className="bnt_item">로맨스</button>
-                </Link>
-                <Link to='/tutorpage'>
-                    <button className="bnt_item">액션</button>
-                </Link>
-                <Link to='/tutorpage'>
-                    <button className="bnt_item">호러</button>
-                </Link>
-            </div>
+const FindTutor = () => {
+  const [currentTab, setCurrentTab] = useState('느와르');
 
-            <div className="content">
-                <Link to='/tutorpage' className="img_wrapper">
-                    <img src="https://via.placeholder.com/340x400" alt="placeholder"/>
-                </Link>
-            
-                <Link to='/tutorpage' className="img_wrapper">
-                    <img src="https://via.placeholder.com/340x400" alt="placeholder"/>
-                </Link>
-                
-                <Link to='/tutorpage' className="img_wrapper">
-                    <img src="https://via.placeholder.com/340x400" alt="placeholder"/>
-                </Link>
-            </div>
-            
-            <div className="parent">
-                <Link to='/next_page'>
-                    <button className="bnt_finish" >다음페이지</button>
-                </Link>
-            </div>
-        </div>
-    );
+  const handleTabChange = (tab) => {
+    setCurrentTab(tab);
+  };
+
+  return (
+    <div>
+      <TabBar currentTab={currentTab} onTabChange={handleTabChange} />
+      <TutorImage genre={currentTab} />
+    </div>
+  );
 };
 
 export default FindTutor;
